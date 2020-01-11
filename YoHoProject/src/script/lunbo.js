59eae2dea4fe4632224657e3dcdee36ec2b3735d
w517521498 <huangqin_11163com@qq.com>
2020-01-11 22:25:49 +0800

    第三次提交

---
+++ YoHoProject/src/script/lunbo.js
@@ -0,0 +1,59 @@
+class lunbo{
+    constructor(){
+        this.datu=document.querySelector('.datu');
+        this.spic=document.querySelectorAll('.spic li');
+        this.bpic=document.querySelectorAll('.bpic li');
+        this.arrowright=document.querySelector('#right');
+        this.arrowleft=document.querySelector('#left');
+        this.index=0;
+        this.timer=null;
+    }
+    init(){
+        for(let i=0;i<this.spic.length;i++){
+            this.spic[i].onmouseover =()=>{
+                this.index=i;
+                this.tabswitch();
+            }
+        };
+        this.arrowright.onclick =()=>{
+            this.rightclick();
+        }
+        this.arrowleft.onclick =()=>{
+            this.leftclick();
+        };
+        this.autoplay();
+        this.datu.onmouseover =()=>{
+            clearInterval(this.timer);
+        }
+        this.datu.onmouseout =()=>{
+            this.autoplay();
+        }
+    }
+    tabswitch(){
+        for(let j=0;j<this.spic.length;j++){
+            this.spic[j].className='';
+            this.bpic[j].className='';
+        }
+        this.spic[this.index].className='active';
+        this.bpic[this.index].className='show';
+    }
+    leftclick(){
+        this.index--;
+        if(this.index<0){
+            this.index=this.spic.length -1;
+        }
+        this.tabswitch();
+    }
+    rightclick(){
+        this.index++;
+        if(this.index >this.spic.length -1){
+            this.index=0;
+        }
+    }
+    autoplay(){
+        this.timer=setInterval(()=>{
+            this.arrowright.onclick()
+        },2000);
+    }
+}
+new lunbo().init();
