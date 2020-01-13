135ed377f48d10666192bd1c6ff7e41ab0a96c41
w517521498 <huangqin_11163com@qq.com>
2020-01-13 22:58:46 +0800

    第五次提交

--- YoHoProject/src/script/nav.js
+++ YoHoProject/src/script/nav.js
@@ -1,13 +1,14 @@
 $('.daohang nav li a').hover(function(){
     $(this).find("span").css("display","block");
     //$('.daohang a span').css("display","block");
-    $('.daohang .fushi .list').css("display","block");
+     $('.daohang .fushi .list').css("display","block");
 },function(){
-    $('.daohang a span').css("display","none");
+    $(this).find("span").css("display","none");
     $('.daohang .fushi .list').css("display","none");
 });
-$('.daohang nav li liang').hover(function(){
-    $('.daohang .fushi .list').css("display","block");
+
+$('#dingbu .dingllan .kf').hover(function(){
+    $(this).find("ul").css("display","block");
 },function(){
-    $('.daohang .fushi .list').css("display","none");
-})
+    $(this).find("ul").css("display","none");
+});
